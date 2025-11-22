6188ab8f4037ae3fc9246f480673c21faf68c1cf
Christopher Wróbel <wrobel1999@gmail.com>
2025-11-22 10:39:09 +0100

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A small development flake with protobuf";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.protobuf
+          ];
+        };
+      });
+}
